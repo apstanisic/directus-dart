d248c5c048b468e4a469f011ee83fd8af3b5071b
pashkadev <mob@halikov-studio.ru>
2021-09-29 11:47:11 +0500

    mocks generate

--- test/src/mock/mock_dio_response.dart
+++ test/src/mock/mock_dio_response.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 
-typedef Res = Future<Response<dynamic>> Function(Invocation);
+typedef Res = Future<Response<Object?>> Function(Invocation);
 
 /// This function is used to set response when mocking [Dio] request.
 ///
 /// Mockito's `thenAnwser` expects function, this wrapper function returns
 /// that function that will return response with data that is passed to [dioResponse].
-Res dioResponse([dynamic data]) {
+Res dioResponse([Object? data]) {
   return (Invocation invocation) async => Response(
         data: data,
         requestOptions: RequestOptions(path: '/'),
