534a87809a28d61e55f73dbd78aed160c02e817d
pashkadev <mob@halikov-studio.ru>
2021-09-29 10:46:44 +0500

    change dynamic to Object?

--- lib/src/modules/fields/field_converter.dart
+++ lib/src/modules/fields/field_converter.dart
@@ -4,9 +4,9 @@ import 'directus_field.dart';
 
 class FieldConverter implements ItemsConverter<DirectusField> {
   @override
-  Map<String, dynamic> toJson(data) => data.toJson();
+  Map<String, Object?> toJson(data) => data.toJson();
 
   @override
-  DirectusField fromJson(Map<String, dynamic> data) =>
+  DirectusField fromJson(Map<String, Object?> data) =>
       DirectusField.fromJson(data);
 }
