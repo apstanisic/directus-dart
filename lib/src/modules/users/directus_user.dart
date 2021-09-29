ee4ff8685ce78fb1029151b389f39022b5a1f742
pashkadev <mob@halikov-studio.ru>
2021-09-29 14:31:55 +0500

    added additionally fields

--- lib/src/modules/users/directus_user.dart
+++ lib/src/modules/users/directus_user.dart
@@ -1,8 +1,3 @@
-import 'package:json_annotation/json_annotation.dart';
-
-part 'directus_user.g.dart';
-
-@JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
 class DirectusUser {
   String? id;
   String? firstName;
@@ -23,6 +18,9 @@ class DirectusUser {
   /// Role is either [String] or [DirectusRole].
   Object? role;
 
+  /// Additional fields
+  Map<String, Object?>? additionally;
+
   DirectusUser({
     this.id,
     this.firstName,
@@ -38,12 +36,86 @@ class DirectusUser {
     this.theme,
     this.status,
     this.role,
+    this.additionally,
   });
 
   /// Used for code generation
-  factory DirectusUser.fromJson(Map<String, Object?> json) =>
-      _$DirectusUserFromJson(json);
+  factory DirectusUser.fromJson(Map<String, Object?> json) {
+    final data = json;
+    return DirectusUser(
+      id: data['id'] as String?,
+      firstName: data['first_name'] as String?,
+      lastName: data['last_name'] as String?,
+      email: data['email'] as String?,
+      password: data['password'] as String?,
+      location: data['location'] as String?,
+      title: data['title'] as String?,
+      description: data['description'] as String?,
+      tags: (data['tags'] as List<Object?>?)?.map((e) => e as String).toList(),
+      avatar: data['avatar'],
+      language: data['language'] as String?,
+      theme: data['theme'] as String?,
+      status: data['status'] as String?,
+      role: data['role'],
+      additionally: data
+        ..remove('id')
+        ..remove('first_name')
+        ..remove('last_name')
+        ..remove('email')
+        ..remove('password')
+        ..remove('location')
+        ..remove('title')
+        ..remove('description')
+        ..remove('tags')
+        ..remove('avatar')
+        ..remove('language')
+        ..remove('theme')
+        ..remove('status')
+        ..remove('role'),
+    );
+  }
 
   /// Used for code generation
-  Map<String, Object?> toJson() => _$DirectusUserToJson(this);
+  Map<String, Object?> toJson() {
+    final val = <String, Object?>{};
+    void writeNotNull(String key, Object? value) {
+      if (value != null) {
+        val[key] = value;
+      }
+    }
+
+    writeNotNull('id', id);
+    writeNotNull('first_name', firstName);
+    writeNotNull('last_name', lastName);
+    writeNotNull('email', email);
+    writeNotNull('password', password);
+    writeNotNull('location', location);
+    writeNotNull('title', title);
+    writeNotNull('description', description);
+    writeNotNull('tags', tags);
+    writeNotNull('avatar', avatar);
+    writeNotNull('language', language);
+    writeNotNull('theme', theme);
+    writeNotNull('status', status);
+    writeNotNull('role', role);
+    return val;
+  }
+
+  @override
+  String toString() => 'DirectusUser('
+      'id: $id, '
+      'firstName: $firstName, '
+      'lastName: $lastName, '
+      'email: $email, '
+      'password: $password, '
+      'location: $location, '
+      'title: $title, '
+      'description: $description, '
+      'tags: $tags, '
+      'avatar: $avatar, '
+      'language: $language, '
+      'theme: $theme, '
+      'status: $status, '
+      'role: $role, '
+      'additionally: $additionally,)';
 }
